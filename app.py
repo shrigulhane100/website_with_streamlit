1ed9e8f4ef2f7fea0e26224db956f2a88c521534
shrigulhane100 <106003535+shrigulhane100@users.noreply.github.com>
2022-11-30 16:41:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,11 @@ st.header('Calculate subtraction of two number.')
 def user_input():
     numnber_1 = st.number_input('Number 1:', min_value=1, max_value=10000000)
     numnber_2 = st.number_input('Number 2:', min_value=1, max_value=10000000)
-    st.write("Subtraction of number is :", numnber_1 - numnber_2)
-
+    if numnber_1<=0 or numnber_2<=0:
+        st.error('Enter number above 0')
+    else: 
+        st.button('Subtract'):
+        st.write("Subtraction of number is :", numnber_1 - numnber_2)
+        st.success('Excellent')
 
 user_input()
