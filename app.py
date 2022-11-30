b005b72c6e5ec13bd4254b693f67062723e12529
shrigulhane100 <106003535+shrigulhane100@users.noreply.github.com>
2022-11-30 15:58:14 +0530

    Create your student website for Data Science
    I created this website with streamlit and hosted it on Huroku.

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.write(""""""
+         # Subtraction of two given numbers
+         """""")
+st.header('Calculate subtraction of two number.')
+
+
+def user_input():
+    numnber_1 = st.number_input('Number 1:', min_value=1, max_value=10000000)
+    numnber_2 = st.number_input('Number 2:', min_value=1, max_value=10000000)
+    st.write("Subtraction of number is :", numnber_1 - numnber_2)
+
+
+user_input()
