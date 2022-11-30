1080444680b7805a24a8eb9cfd74428d331239a9
shrigulhane100 <106003535+shrigulhane100@users.noreply.github.com>
2022-11-30 16:57:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ def user_input():
     numnber_1 = st.number_input('Number 1:', min_value=1, max_value=10000000)
     numnber_2 = st.number_input('Number 2:', min_value=1, max_value=10000000)
  
-    st.button('Subtract')
-    st.write("Subtraction of number is :", numnber_1 - numnber_2)
-
+    if st.button('Subtract'):
+         st.write("Subtraction of number is :", numnber_1 - numnber_2)
+         
     
 user_input()
