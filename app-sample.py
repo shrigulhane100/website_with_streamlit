44faf1f653a7d196f06e634db20b7299fca089e3
Shriyash Gulhane <106003535+shrigulhane100@users.noreply.github.com>
2023-04-08 17:51:25 +0530

    cc

---
+++ app-sample.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import time
+
+st.write(""""""
+         # Subtraction of two given numbers
+         """""")
+st.header('Calculate subtraction of two number.')
+
+
+def user_input():
+    numnber_1 = st.number_input('Number 1:', min_value=1, max_value=10000000)
+    numnber_2 = st.number_input('Number 2:', min_value=1, max_value=10000000)
+ 
+    if st.button('Subtract'):
+         st.write("Subtraction of number is :", numnber_1 - numnber_2)
+         
+    
+user_input()
